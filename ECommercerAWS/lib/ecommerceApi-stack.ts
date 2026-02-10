7eb204f44e47d320138072bac3489b84ff60ad08
Raphael W B A Melo <raphaanjos1@gmail.com>
2026-02-09 21:29:06 -0300

    feat: Integrando o API Gateway com a função Lambda de pesquisa de produtos

--- ECommercerAWS/lib/ecommerceApi-stack.ts
+++ ECommercerAWS/lib/ecommerceApi-stack.ts
@@ -4,15 +4,22 @@ import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as cwlogs from "aws-cdk-lib/aws-logs";
 import { Construct } from "constructs";
 
+interface EcommerceApiStackProps extends cdk.StackProps {
+    productsFatchHandler: lambdaNodeJS.NodejsFunction;
+}
 
 export class EcommerceApiStack extends cdk.Stack {
 
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    constructor(scope: Construct, id: string, props?: EcommerceApiStackProps) {
         super(scope, id, props);
 
         const api = new apigateway.RestApi(this, "EcommerceApi", {
             restApiName: "Ecommerce Api",
             description: "Api para o ecommerce"
         })
+
+        const productsFetchIntegration = new apigateway.LambdaIntegration(props!.productsFatchHandler);
+        const productsResource = api.root.addResource("products");
+        productsResource.addMethod("GET", productsFetchIntegration);
     }
 }
