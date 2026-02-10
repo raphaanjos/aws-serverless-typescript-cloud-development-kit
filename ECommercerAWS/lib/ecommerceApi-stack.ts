1c5f2ffbb10e20a1c225e9e0bd661c3ea44c638c
Raphael W B A Melo <raphaanjos1@gmail.com>
2026-02-09 21:47:23 -0300

    feat: gerando logs no API Gateway

--- ECommercerAWS/lib/ecommerceApi-stack.ts
+++ ECommercerAWS/lib/ecommerceApi-stack.ts
@@ -13,9 +13,25 @@ export class EcommerceApiStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: EcommerceApiStackProps) {
         super(scope, id, props);
 
+        const logGroup = new cwlogs.LogGroup(this, "EcommerceApiLogGroup");
         const api = new apigateway.RestApi(this, "EcommerceApi", {
             restApiName: "Ecommerce Api",
-            description: "Api para o ecommerce"
+            description: "Api para o ecommerce",
+            deployOptions: {
+                // Inclui o log de acesso para cada requisição feita à API Gateway, enviando os logs para o Log Group criado
+                accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+                accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+                    httpMethod: true,
+                    ip: true,
+                    protocol: true,
+                    requestTime: true,
+                    resourcePath: true,
+                    responseLength: true,
+                    status: true,
+                    caller: true,
+                    user: true
+                })
+            }
         })
 
         const productsFetchIntegration = new apigateway.LambdaIntegration(props!.productsFatchHandler);
