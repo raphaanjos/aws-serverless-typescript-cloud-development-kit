95190120e95839c4ce92ab546d8eb56c8d130163
Raphael W B A Melo <raphaanjos1@gmail.com>
2026-02-09 21:06:08 -0300

    feat: Criando a stack do API Gateway

---
+++ ECommercerAWS/lib/ecommerceApi-stack.ts
@@ -0,0 +1,18 @@
+import * as cdk from "aws-cdk-lib";
+import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as cwlogs from "aws-cdk-lib/aws-logs";
+import { Construct } from "constructs";
+
+
+export class EcommerceApiStack extends cdk.Stack {
+
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const api = new apigateway.RestApi(this, "EcommerceApi", {
+            restApiName: "Ecommerce Api",
+            description: "Api para o ecommerce"
+        })
+    }
+}
