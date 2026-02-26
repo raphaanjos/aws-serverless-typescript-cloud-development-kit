1b7d5f8c1e30db6aec95f1a55f7bc62623f10b30
Raphael W B A Melo <raphaanjos1@gmail.com>
2026-02-25 21:44:02 -0300

    feat: Criando a tabela do DynamoDB com o AWS CDK

--- ECommercerAWS/lib/productsApp-stack.ts
+++ ECommercerAWS/lib/productsApp-stack.ts
@@ -1,6 +1,10 @@
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambdaNodeJS from 'aws-cdk-lib/aws-lambda-nodejs';
+// https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_lambda_nodejs.NodejsFunction.html
 import * as cdk from 'aws-cdk-lib';
+// https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_dynamodb.Table.html
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+
 import { Construct } from 'constructs';
 
 /**
@@ -10,12 +14,25 @@ export class ProductsAppStack extends cdk.Stack {
 
     // productsHandler é uma função Lambda que será usada para lidar com as requisições relacionadas aos produtos
     readonly productsHandler: lambdaNodeJS.NodejsFunction;
+    readonly productsDdb: dynamodb.Table;
 
     // pros são propriedades que podem ser passadas para o construtor da stack
     // scope é o escopo onde a stack será definida, geralmente é a própria stack ou um app
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
+        this.productsDdb = new dynamodb.Table(this, 'ProductsDdb', {
+            tableName: 'products',
+            removalPolicy: cdk.RemovalPolicy.DESTROY, // Apaga a tabela quando a stack for destruída (não recomendado para produção)
+            partitionKey: {
+                name: 'id',
+                type: dynamodb.AttributeType.STRING
+            },
+            billingMode: dynamodb.BillingMode.PROVISIONED,
+            readCapacity: 1,
+            writeCapacity: 1
+        })
+        
         this.productsHandler = new lambdaNodeJS.NodejsFunction(this, 'ProductsHandler', {
             runtime: lambda.Runtime.NODEJS_20_X,
             memorySize: 512,
